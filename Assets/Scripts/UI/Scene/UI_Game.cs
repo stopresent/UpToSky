a7f8a2cb244ed8478466e252b8c4cb48c1fa0465
Stopresent <mamy4939@gmail.com>
2023-02-16 21:02:55 +0900

    사운드 추가

--- Assets/Scripts/UI/Scene/UI_Game.cs
+++ Assets/Scripts/UI/Scene/UI_Game.cs
@@ -6,7 +6,7 @@ using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class UI_Game : UI_Scene
 {
-    GameObject _player;
+    //public GameObject _player;
 
     enum Buttons
     {
@@ -21,8 +21,6 @@ public class UI_Game : UI_Scene
     enum Images
     {
         ScoreImage,
-        //StoryBG,
-        //ScoreBG,
     }
 
     enum GameObjects
@@ -40,30 +38,30 @@ public class UI_Game : UI_Scene
     {
         if (Managers.Game.Mode != Define.Mode.StoryMode) return;
 
-        if (_player.transform.position.y < 100)
-        {
-            // 도시 브금
-        }
-        else if (_player.transform.position.y < 500)
-        {
-            // 에베레스트 브금
-        }
-        else if (_player.transform.position.y < 1000)
-        {
-            // 하늘 세계 브금
-        }
-        else if (_player.transform.position.y < 2000)
-        {
-            // 성층권 브금
-        }
-        else if (_player.transform.position.y < 5000)
-        {
-            // 열권 브금
-        }
-        else
-        {
-            // 우주 브금
-        }
+        //if (_player.transform.position.y < 100)
+        //{
+        //    // 도시 브금
+        //}
+        //else if (_player.transform.position.y < 500)
+        //{
+        //    // 에베레스트 브금
+        //}
+        //else if (_player.transform.position.y < 1000)
+        //{
+        //    // 하늘 세계 브금
+        //}
+        //else if (_player.transform.position.y < 2000)
+        //{
+        //    // 성층권 브금
+        //}
+        //else if (_player.transform.position.y < 5000)
+        //{
+        //    // 열권 브금
+        //}
+        //else
+        //{
+        //    // 우주 브금
+        //}
 
     }
 
@@ -72,15 +70,13 @@ public class UI_Game : UI_Scene
         if (base.Init() == false)
             return false;
 
-        _player = GameObject.Find("Player").gameObject;
+        //_player = GameObject.Find("Player").gameObject;
 
         BindButton(typeof(Buttons));
         BindText(typeof(Texts));
         BindImage(typeof(Images));
 
         GetButton((int)Buttons.SettingBtn).gameObject.BindEvent(Setting);
-        //GetImage((int)Images.StoryBG).gameObject.SetActive(false);
-        //GetImage((int)Images.ScoreBG).gameObject.SetActive(false);
 
         // TODO 배경에 따라 브금이 바뀌어야 함
         // TODO 모드에 따라 게임씬 바뀜
@@ -102,7 +98,6 @@ public class UI_Game : UI_Scene
         Managers.Sound.Clear();
         Managers.Sound.Play("BGM/Sound_Lazy", Sound.Bgm);
         GameObject.Find("Test_BG").gameObject.GetComponent<SpriteRenderer>().sprite = Managers.Resource.Load<Sprite>("Sprites/BG/StoryModeBGImage");
-        //GetImage((int)Images.StoryBG).gameObject.SetActive(true);
 
         // 배경은 높이에 따라 달라지므로 높이를 측정하여 특정 높이가 됐을 때 브금 변경?
     }
@@ -111,9 +106,8 @@ public class UI_Game : UI_Scene
     {
         // TODO 무한 배경 OR 우주니까 배경 고정?
         Managers.Sound.Clear();
-        Managers.Sound.Play("BGM/Sound_Lazy", Sound.Bgm); // TODO 스코어 모드 전용 브금으로 교체
+        Managers.Sound.Play("BGM/Sound_GalaxyBlues", Sound.Bgm); // TODO 스코어 모드 전용 브금으로 교체
         GameObject.Find("Test_BG").gameObject.GetComponent<SpriteRenderer>().sprite = Managers.Resource.Load<Sprite>("Sprites/BG/ScoreModeBGImage");
-        //GetImage((int)Images.ScoreBG).gameObject.SetActive(true);
     }
 
     void Setting()
